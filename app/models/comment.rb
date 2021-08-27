8b7d132d10ccc52b1b86d119e9e827c82efce38d
ErizoMA <emarceloa@uni.pe>
2021-08-27 16:45:25 +0000

    Add validations to models and a avatar to user model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,7 @@ class Comment < ApplicationRecord
   # Associations
   belongs_to :user
   belongs_to :tweet, counter_cache: true
+
+  # Validations
+  validates :body, presence: true, length: { maximum: 140 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,16 @@ class User < ApplicationRecord
   # Associations
   has_many :tweets, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  # Validations
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+
+  validates :email, uniqueness: true
+  validates :email, format: { with: VALID_EMAIL_REGEX }, allow_blank: false
+  validates :username, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates :password, numericality: { greater_than_or_equal_to: 6 }
+
+  # Active Storage
+  has_one_attached :avatar
 end
