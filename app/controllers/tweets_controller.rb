e881a58fb1479014535f611eb51dfbd3131af962
ErizoMA <emarceloa@uni.pe>
2021-08-27 19:53:09 +0000

    Add create method to tweet controller and also added to principal view

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -6,4 +6,20 @@ class TweetsController < ApplicationController
   def show
     @tweet = Tweet.find(params[:id])
   end
+
+  def new
+    # @tweet = authorize Company.new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new(body: params[:body])
+    @tweet.user = User.first
+
+    if @tweet.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'tweets#index'
 
-  resources :tweets, only: %i[show index]
+  resources :tweets
 end
