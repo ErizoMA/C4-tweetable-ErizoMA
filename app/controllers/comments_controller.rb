74dac5fa4ffd64069e5bc9e1330130e374989050
ErizoMA <emarceloa@uni.pe>
2021-08-28 01:20:53 +0000

    Add funcionality to delete comment icon

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,4 +12,16 @@ class CommentsController < ApplicationController
       render :new
     end
   end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
 end
