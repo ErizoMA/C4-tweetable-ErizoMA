a522ee89bf5cd11b5c17a5b698d449dfcf809773
ErizoMA <emarceloa@uni.pe>
2021-08-28 07:02:37 +0000

    Add styles to edit comment tweet

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,26 +20,35 @@ user5 = User.create(email: 'user5@mail.com', username: 'user5', name: 'Usuario 5
 user5.avatar.attach(io: File.open("app/assets/images/user_5.png"), filename: "user_5.png")
 user6 = User.create(email: 'user6@mail.com', username: 'user6', name: 'Usuario 6', password: 'password')
 user6.avatar.attach(io: File.open("app/assets/images/user_6.png"), filename: "user_6.png")
-user7 = User.create(email: 'user7@mail.com', username: 'user7', name: 'Usuario 7', password: 'password')
+user7 = User.create(email: 'tester@mail.com', username: 'tester', name: 'Usuario 7', password: 'tester')
 user7.avatar.attach(io: File.open("app/assets/images/user_7.png"), filename: "user_7.png")
 puts "Creating tweets"
-tweet_1 = Tweet.create(body:"Tweet 1 creado por usuario 7",user:User.last)
+tweet_1 = Tweet.create(body:"Hi my email es tester@mail.com and my password: tester, test the app with me ...",user:User.last)
 tweet_2 = Tweet.create(body:"Tweet 2 creado por usuario 1",user:User.first)
 tweet_3 = Tweet.create(body:"Tweet 3 creado por usuario 2",user:User.second)
 
 puts "Creating comments"
 
-Comment.create(user:User.first,tweet:Tweet.first,body:"Usuario 1 responde al Tweet 1 creado por Usuario 7")
-Comment.create(user:User.second,tweet:Tweet.first,body:"Usuario 2 responde al Tweet 1 creado por Usuario 7")
-Comment.create(user:User.third,tweet:Tweet.first,body:"Usuario 3 responde al Tweet 1 creado por Usuario 7")
+Comment.create(user:User.first,tweet:Tweet.first,body:"Usuario 1 responde al Tweet 1 creado por tester")
+Comment.create(user:User.last,tweet:Tweet.first,body:"Tester responde al Tweet")
+Comment.create(user:User.second,tweet:Tweet.first,body:"Usuario 2 responde al Tweet 1 creado por tester")
+Comment.create(user:User.third,tweet:Tweet.first,body:"Usuario 3 responde al Tweet 1 creado por tester")
+Comment.create(user:User.last,tweet:Tweet.first,body:"Tester responde al Tweet")
 
+Comment.create(user:User.last,tweet:Tweet.second,body:"Tester por aqui tambien")
 Comment.create(user:User.first,tweet:Tweet.second,body:"Usuario 1 responde al Tweet 2 creado por Usuario 1")
 Comment.create(user:User.second,tweet:Tweet.second,body:"Usuario 2 responde al Tweet 2 creado por Usuario 1")
 Comment.create(user:User.third,tweet:Tweet.second,body:"Usuario 3 responde al Tweet 2 creado por Usuario 1")
+Comment.create(user:User.last,tweet:Tweet.second,body:"Tester responde al Tweet")
+
 
 Comment.create(user:User.first,tweet:Tweet.third,body:"Usuario 1 responde al Tweet 3 creado por Usuario 2")
 Comment.create(user:User.second,tweet:Tweet.third,body:"Usuario 2 responde al Tweet 3 creado por Usuario 2")
+Comment.create(user:User.last,tweet:Tweet.third,body:"Tester responde al Tweet")
 Comment.create(user:User.third,tweet:Tweet.third,body:"Usuario 3 responde al Tweet 3 creado por Usuario 2")
+Comment.create(user:User.last,tweet:Tweet.third,body:"Tester aun por aqui")
+
+
 
 puts "Seeds created..."
 
