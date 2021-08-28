1d82653918277f4d1b4bbc390ddf8127c82cf7b4
ErizoMA <emarceloa@uni.pe>
2021-08-28 01:00:00 +0000

    Add funcionality to edit and delete tweet icon

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -28,13 +28,22 @@ class TweetsController < ApplicationController
     redirect_to root_path
   end
 
-  def edit; end
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
 
   def update
+    @tweet = Tweet.find(params[:id])
     if @tweet.update(tweet_params)
       redirect_to tweets_path
     else
       render :edit
     end
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:body)
   end
 end
